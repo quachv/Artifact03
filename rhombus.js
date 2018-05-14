b117fcce41b6bb6e09db72506b8cb2da8ceb4f6d
quachv <v.quach@bellevuecollege.edu>
2018-05-13 21:25:51 -0700

    rhom edit

--- rhombus.js
+++ rhombus.js
@@ -55,53 +55,31 @@ function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
 document.getElementById("upRight").innerHTML = rLine;
 }
 
-function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
-  var rLine ="";
-  for (i=1; i<=pHeight; i++){
-    rLine += "<p>";
-    //Fill in left side of every row with blanks
-    for(x=1; x<=pHeight-i; x++){
-      rLine += "<span class='space'>" + pSymbol + "</span>";
-    }
-    //Create each line on the Rhombus
-		for(j=x; j<=pHeight; j++){
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+	var rLine ="";
+	for (i=pHeight; i > 0; i--){
+		rLine +="<p>";
+		//Fill in left side of every row with blanks
+		for(x=1; x<=pHeight-i; x++){
+			rLine += "<span class='space'>" + pSymbol + "</span>";
+		}
+		//Create each line on the Rhombus
+		for(j=0; j<i; j++){
 			//Is the position even or odd so we change the color
 			if (j%2)
 				//even
-				rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+				rLine += "<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
 			else
 				//odd
 				rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
 		}
-    rLine +="</p>";
-    // console.log(rLine);
-  }
-document.getElementById("upLeft").innerHTML = rLine;
+	rLine += "</p>";
+	// console.log(rLine);
+	}
+document.getElementById("downLeft").innerHTML = rLine;
 }
 
-
-function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
-  var rLine ="";
-  for (i=0;i<pHeight;i++){
-    rLine +="<p>";
-    //Create each line on the Rhombus
-      for(j=0;j<=i;j++){
-        //Is the position even or odd so we change the color
-        if (j%2)
-        //even
-        rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
-        else
-        //odd
-        rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
-      }
-    rLine +="</p>";
-    // console.log(rLine);
-  }
-document.getElementById("upRight").innerHTML = rLine;
-}
-
-
-/*function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
+function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
 	var rLine ="";
 	for (i=pHeight; i > 0; i--){
 		rLine +="<p>";
@@ -120,4 +98,3 @@ document.getElementById("upRight").innerHTML = rLine;
 	}
 document.getElementById("downRight").innerHTML = rLine;
 }
-*/
